f21b5d012fdc12d16462d413cb2caf4a7cb7fe1c
Miraj Shah <mirajholmes@gmail.com>
2022-12-04 01:35:48 +0530

    mul op for tensor works !

--- src/tensor/ops/binary_ops.rs
+++ src/tensor/ops/binary_ops.rs
@@ -81,6 +81,24 @@ macro_rules! impl_std_binary_ops {
                 tensor
             }
         }
+
+        impl<'a, S, Dtype> std::ops::$op_trait<TensorView<'a, S, Dtype>> for &Tensor<S, Dtype>
+        where
+            S: Dimension,
+            Dtype: Copy + std::ops::$op_trait<Dtype, Output = Dtype>,
+        {
+            type Output = Tensor<S, Dtype>;
+
+            fn $op(self, other: TensorView<S, Dtype>) -> Self::Output {
+                assert!(self.shape() == other.shape());
+                let mut data = Vec::with_capacity(self.data.len());
+                for (a, b) in zip(self.data.iter(), other.data.iter()) {
+                    data.push(*a $op_symbol **b);
+                }
+                let tensor = Tensor::from_vec(data, self.dim.clone());
+                tensor
+            }
+        }
     };
 }
 
@@ -120,7 +138,7 @@ pub trait TensorBinaryOps<Rhs> {
     type Output;
     fn add(&self, rhs: &Rhs) -> Self::Output;
     fn sub(&self, rhs: &Rhs) -> Self::Output;
-    // fn mul(&self, rhs: &Self) -> Self::Output;
+    fn mul(&self, rhs: &Rhs) -> Self::Output;
 }
 
 impl<L, R, Dtype> TensorBinaryOps<Tensor<R, Dtype>> for Tensor<L, Dtype>
@@ -135,24 +153,24 @@ where
         let (out, mut backops) = impl_binary_ops_with_broadcast!(self, rhs, +);
         let o_id = out.id;
 
-        let lhs = (self.id, self.dim());
-        let rhs = (rhs.id, rhs.dim());
+        let l = (self.id, self.dim());
+        let r = (rhs.id, rhs.dim());
         if backops.is_some() {
             backops.as_mut().unwrap().add_backward_op(move |grad| {
                 let (grad_lhs, grad_rhs, grad_out): (
                     &mut Tensor<_, Dtype>,
                     &mut Tensor<_, Dtype>,
                     &Tensor<DimMaxOf<L, R>, Dtype>,
-                ) = grad.mmr_grad(lhs.clone(), rhs.clone(), o_id);
+                ) = grad.mmr_grad(l.clone(), r.clone(), o_id);
 
-                if lhs.1.shape() == rhs.1.shape() {
+                if l.1.shape() == r.1.shape() {
                     let g_out = grad_out.into_dimensionality::<L>();
                     *grad_lhs = grad_lhs.clone() + g_out;
                     let g_out = grad_out.into_dimensionality::<R>();
                     *grad_rhs = grad_rhs.clone() + g_out;
                 } else {
-                    *grad_lhs = grad_lhs.clone() + reduced_grad(lhs.1, grad_out);
-                    *grad_rhs = grad_rhs.clone() + reduced_grad(rhs.1, grad_out);
+                    *grad_lhs = grad_lhs.clone() + reduced_grad(l.1, grad_out);
+                    *grad_rhs = grad_rhs.clone() + reduced_grad(r.1, grad_out);
                 }
             });
         }
@@ -165,23 +183,23 @@ where
         let (out, mut backops) = impl_binary_ops_with_broadcast!(self, rhs, -);
         let o_id = out.id;
 
-        let lhs = (self.id, self.dim());
-        let rhs = (rhs.id, rhs.dim());
+        let l = (self.id, self.dim());
+        let r = (rhs.id, rhs.dim());
         if backops.is_some() {
             backops.as_mut().unwrap().add_backward_op(move |grad| {
                 let (grad_lhs, grad_rhs, grad_out): (
                     &mut Tensor<_, Dtype>,
                     &mut Tensor<_, Dtype>,
                     &Tensor<DimMaxOf<L, R>, Dtype>,
-                ) = grad.mmr_grad(lhs.clone(), rhs.clone(), o_id);
-                if lhs.1.shape() == rhs.1.shape() {
+                ) = grad.mmr_grad(l.clone(), r.clone(), o_id);
+                if l.1.shape() == r.1.shape() {
                     let g_out = grad_out.into_dimensionality::<L>();
                     *grad_lhs = grad_lhs.clone() + g_out;
                     let g_out = grad_out.into_dimensionality::<R>();
                     *grad_rhs = grad_rhs.clone() - g_out;
                 } else {
-                    *grad_lhs = grad_lhs.clone() + reduced_grad(lhs.1, grad_out);
-                    *grad_rhs = grad_rhs.clone() - reduced_grad(rhs.1, grad_out);
+                    *grad_lhs = grad_lhs.clone() + reduced_grad(l.1, grad_out);
+                    *grad_rhs = grad_rhs.clone() - reduced_grad(r.1, grad_out);
                 }
             });
         }
@@ -190,36 +208,43 @@ where
         out
     }
 
-    // fn mul(&self, rhs: &Self) -> Self::Output {
-    //     let (out, mut backops) = impl_binary_ops_with_broadcast!(self, rhs, *);
-    //     let o_id = out.id;
+    fn mul(&self, rhs: &Tensor<R, Dtype>) -> Self::Output {
+        let (out, mut backops) = impl_binary_ops_with_broadcast!(self, rhs, *);
+        let o_id = out.id;
 
-    //     let lhs = (self.id, self.dim());
-    //     let rhs = (rhs.id, rhs.dim());
-    //     if backops.is_some() {
-    //         backops.as_mut().unwrap().add_backward_op(move |grad| {
-    //             let (grad_lhs, grad_rhs, grad_out): (
-    //                 &mut Tensor<_, Dtype>,
-    //                 &mut Tensor<_, Dtype>,
-    //                 &Tensor<_, Dtype>,
-    //             ) = grad.mmr_grad(l, r, o_id);
-    //             if l.1.is_owned() {
-    //                 let _lhs = l.1.take_owned();
-    //                 let _rhs = r.1.take_owned();
-    //                 *grad_lhs = grad_lhs.clone() + grad_out * &_rhs;
-    //                 *grad_rhs = grad_rhs.clone() + grad_out * &_lhs;
-    //             } else {
-    //                 let _lhs = l.1.take_view();
-    //                 let _rhs = r.1.take_view();
-    //                 *grad_lhs = grad_lhs.clone() + grad_out * _rhs;
-    //                 *grad_rhs = grad_rhs.clone() + grad_out * _lhs;
-    //             }
-    //         });
-    //     }
+        let l = (self.id, self.dim());
+        let r = (rhs.id, rhs.dim());
+        let lhs_clone = self.clone();
+        let rhs_clone = rhs.clone();
+        if backops.is_some() {
+            backops.as_mut().unwrap().add_backward_op(move |grad| {
+                let (grad_lhs, grad_rhs, grad_out): (
+                    &mut Tensor<_, Dtype>,
+                    &mut Tensor<_, Dtype>,
+                    &Tensor<DimMaxOf<L, R>, Dtype>,
+                ) = grad.mmr_grad(l.clone(), r.clone(), o_id);
+                if l.1.shape() == r.1.shape() {
+                    let g_out = grad_out.into_dimensionality::<L>();
+                    let rhs_clone = rhs_clone.into_dimensionality::<L>();
+                    *grad_lhs = grad_lhs.clone() + g_out * rhs_clone;
+                    let g_out = grad_out.into_dimensionality::<R>();
+                    let lhs_clone = lhs_clone.into_dimensionality::<R>();
+                    *grad_rhs = grad_rhs.clone() + g_out * lhs_clone;
+                } else {
+                    let rhs_broadcast = rhs_clone.broadcast(grad_out.dim());
+                    let lhs_broadcast = lhs_clone.broadcast(grad_out.dim());
 
-    //     out.put_backward_ops(backops);
-    //     out
-    // }
+                    let lhs_local_grad = grad_out * rhs_broadcast;
+                    *grad_lhs = grad_lhs.clone() + reduced_grad(l.1, &lhs_local_grad);
+                    let rhs_local_grad = grad_out * lhs_broadcast;
+                    *grad_rhs = grad_rhs.clone() + reduced_grad(r.1, &rhs_local_grad);
+                }
+            });
+        }
+
+        out.put_backward_ops(backops);
+        out
+    }
 }
 
 #[cfg(test)]
@@ -231,6 +256,17 @@ mod tests {
         let t1 = Tensor::from_vec(vec![5, 1, 3], [3, 1]).requires_grad(true);
         let t2 = Tensor::from_vec(vec![1, 2, 3, 4, 5, 6], [2, 1, 3]).requires_grad(true);
         let c = t1.add(&t2);
+        let grads = c.backward();
+
+        println!("t1 grad\n{:#?}\n\n", grads.grad(&t1));
+        println!("t2 grad\n{:#?}\n\n", grads.grad(&t2));
+    }
+
+    #[test]
+    fn mul_tensors() {
+        let t1 = Tensor::from_vec(vec![5, 1, 3], [3, 1]).requires_grad(true);
+        let t2 = Tensor::from_vec(vec![1, 2, 3, 4, 5, 6], [2, 1, 3]).requires_grad(true);
+        let c = t1.mul(&t2);
         let grads = c.backward();
 
         println!("t1 grad\n{:#?}\n\n", grads.grad(&t1));
